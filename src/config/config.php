2c540b57a55d3991f229eaf268c2f04f9aa02600
pamela192003 <rojasataupillcopamels@gmail.com>
2025-10-01 23:27:26 -0500

    pam

--- src/config/config.php
+++ src/config/config.php
@@ -5,6 +5,5 @@ const BD_USER = 'root';
 const BD_PASSWORD = '';
 const BD_CHARSET = 'utf8mb4';
 
-
 const BASE_URL = 'http://localhost/hotel_reservas/';
 const BASE_URL_SERVER = 'http://localhost/hotel_reservas/';
