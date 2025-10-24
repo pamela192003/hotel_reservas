3909f581d3a04d07251720272def74aeec415c22
pamela192003 <rojasataupillcopamels@gmail.com>
2025-10-23 22:47:50 -0500

    submit

--- src/control/apiController.php
+++ src/control/apiController.php
@@ -1,17 +1,38 @@
 <?php
-session_start();
-require_once('../model/admin-sesionModel.php');
 require_once('../model/admin-apiModel.php');
-require_once('../model/adminModel.php');
-
 $tipo = $_GET['tipo'];
-
 //instanciar la clase categoria model
-$objSesion = new SessionModel();
 $objApi = new ApiModel();
-$objAdmin = new AdminModel();
 
-$token = $_POST['token'];
+$token = trim($_POST['token']) ?? '';
+
+if(empty($token)){
+    $arr_Respuesta = array('status' => false, 'mensaje' => 'Acceso no autorizado - token requerido');
+    echo json_encode($arr_Respuesta);
+    exit;
+}
+//validar token
+$tokenn = explode("-", $token);
+if($tokenn[2] == '' || count($tokenn) < 3){
+    $arr_Respuesta = array('status' => false, 'mensaje' => 'Acceso no autorizado - token invalido');
+    echo json_encode($arr_Respuesta);
+    exit;
+}
+
+$id_cliente = $tokenn[2];
+$estadoCliente = $objApi->validarEstadoCliente($id_cliente);
+if($estadoCliente != 1){
+    $arr_Respuesta = array('status' => false, 'mensaje' => 'Cliente API inactivo');
+    echo json_encode($arr_Respuesta);
+    exit;
+}
+$validarClienteToken = $objApi->validarClienteToken($id_cliente , $token);
+if(!$validarClienteToken){
+    $arr_Respuesta = array('status' => false, 'mensaje' => 'Acceso no autorizado - token inactivo o invalido');
+    echo json_encode($arr_Respuesta);
+    exit;
+}
+
 
 if($tipo == "listarReservas"){
         $arrReservas = $objApi->listarReservas();
@@ -50,6 +71,47 @@ if($tipo == "listarReservas"){
              $arr_Respuesta = array('status' => false, 'mensaje' => 'error de sistema');
         }
     echo json_encode($arr_Respuesta);
+}
+
+if($tipo == "listarReservasPorEstado"){
+    $estado = trim($_POST['status']) ?? '';
+        $arrReservas = $objApi->listarReservasPorEstado($estado);
+        if($arrReservas){
+                 for ($i=0; $i < count($arrReservas); $i++) {
+                switch ($arrReservas[$i]->estado) {
+                    case 'pendiente':
+                    $arrReservas[$i]->status = '<span class="badge bg-warning text-dark">Pendiente</span>';
+                        break;
+                    case 'confirmada':
+                    $arrReservas[$i]->status = '<span class="badge bg-success">Confirmada</span>';
+                        break;
+                    case 'cancelada':
+                    $arrReservas[$i]->status = '<span class="badge bg-danger">Cancelada</span>';
+                        break;    
+                    case 'finalizada':
+                    $arrReservas[$i]->status = '<span class="badge bg-secondary">Finalizada</span>';
+                    break;
+                }
+
+                $usuario = $objApi->buscarUsuarioById($arrReservas[$i]->id_usuario);
+                $arrReservas[$i]->username = $usuario->nombre;
+
+                $opciones = '<button class="btn btn-sm btn-outline-warning" title="Editar" onclick="listarReserva('.$arrReservas[$i]->id_reserva.');" data-bs-toggle="modal" data-bs-target="#modalActualizarReserva">
+                                    <i class="bi bi-pencil"></i>
+                                </button>
+                                <button class="btn btn-sm btn-outline-danger" title="Eliminar">
+                                    <i class="bi bi-trash"></i>
+                                </button>';
+                $arrReservas[$i]->options = $opciones;
+            }
+           $arr_Respuesta['status'] = true;           
+           $arr_Respuesta['mensaje'] = 'exit';           
+           $arr_Respuesta['contenido'] = $arrReservas;           
+        }else{
+             $arr_Respuesta = array('status' => false, 'mensaje' => 'error de sistema');
+        }
+    echo json_encode($arr_Respuesta);
+
 }
 
 ?>
--- src/model/admin-apiModel.php
+++ src/model/admin-apiModel.php
@@ -10,7 +10,21 @@ class ApiModel
         $this->conexion = new Conexion();
         $this->conexion = $this->conexion->connect();
     }
+    public function listarReservasPorEstado($estado)
+    {
+        $respuesta = array();
+        $sql = $this->conexion->query("SELECT * FROM reservas WHERE estado='$estado'");
+        while ($objeto = $sql->fetch_object()) {
+            array_push($respuesta, $objeto);
+        }
+        return $respuesta;
+    }
 
+    public function validarEstadoCliente($id_cliente){
+        $sql = $this->conexion->query("SELECT estado FROM client_api WHERE id='$id_cliente'");
+        $sql = $sql->fetch_object();
+        return $sql->estado;
+    }
     public function listarReservas(){
         $respuesta = array();
         $sql = $this->conexion->query("SELECT * FROM reservas");
@@ -19,85 +33,21 @@ class ApiModel
         }
         return $respuesta;
     }
-    public function registrarUsuario($nombre, $apellido, $usuario, $telefono, $passhash,$rol){
-        $sql = $this->conexion->query("INSERT INTO usuarios (nombre, apellido, usuario, telefono, password, rol) VALUES ('$nombre',' $apellido', '$usuario',' $telefono', '$passhash','$rol')");
-        if ($sql) {
-            $sql = $this->conexion->insert_id;
-        } else {
-            $sql = 0;
-        }
-        return $sql;
-    }
-    public function actualizarUsuario($iduser,$nombre,$apellido,$usuario,$telefono,$rol,$estado)
-    {
-        $sql = $this->conexion->query("UPDATE usuarios SET nombre='$nombre',apellido='$apellido',usuario='$usuario',telefono='$telefono',rol='$rol',activo ='$estado' WHERE id_usuario='$iduser'");
-        return $sql;
-    }
+
   public function buscarUsuarioById($id)
     {
         $sql = $this->conexion->query("SELECT * FROM usuarios WHERE id_usuario='$id'");
         $sql = $sql->fetch_object();
         return $sql;
     }
-      public function buscarUsuarioByUsuarioName($usuario)
-    {
-        $sql = $this->conexion->query("SELECT * FROM usuarios WHERE usuario='$usuario'");
-        $sql = $sql->fetch_object();
-        return $sql;
-    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-    public function actualizarPassword($id, $password)
-    {
-        $sql = $this->conexion->query("UPDATE usuarios SET password ='$password' WHERE id='$id'");
-        return $sql;
-    }
-    public function updateResetPassword($id, $token, $estado) {
-        $sql = $this->conexion->query("UPDATE usuarios SET token_password ='$token' , reset_password ='$estado' WHERE id='$id'");
-        return $sql;
-    }
-    public function buscarUsuarioByDni($dni)
-    {
-        $sql = $this->conexion->query("SELECT * FROM usuarios WHERE dni='$dni'");
-        $sql = $sql->fetch_object();
-        return $sql;
-    }
-
-    
-    public function buscarUsuarioByNomAp($nomap)
-    {
-        $sql = $this->conexion->query("SELECT * FROM usuarios WHERE nombres_apellidos='$nomap'");
-        $sql = $sql->fetch_object();
-        return $sql;
-    }
-    public function buscarUsuarioByApellidosNombres_like($dato)
-    {
-        $sql = $this->conexion->query("SELECT * FROM usuarios WHERE nombres_apellidos LIKE '%$dato%'");
-        $sql = $sql->fetch_object();
-        return $sql;
-    }
-    public function buscarUsuariosOrdenados()
-    {
-        $arrRespuesta = array();
-        $sql = $this->conexion->query("SELECT * FROM usuarios WHERE estado='1' ORDER BY nombres_apellidos ASC ");
-        while ($objeto = $sql->fetch_object()) {
-            array_push($arrRespuesta, $objeto);
+    public function validarClienteToken($id_cliente , $token){
+        $sql = $this->conexion->query("SELECT * FROM tokens_api WHERE id_client_api='$id_cliente' AND token='$token' AND estado=1");
+        if($sql->num_rows > 0){
+            return true;
+        }else{
+            return false;
         }
-        return $arrRespuesta;
     }
- 
-
-
 
 }
